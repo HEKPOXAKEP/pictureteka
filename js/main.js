a966ec10d05c44962070083298f8fa41100b7318
Tosh Judeinov <hellish.engineer@gmail.com>
2023-11-22 04:04:00 +0300

    Creating and initializing an App class object

--- js/main.js
+++ js/main.js
@@ -2,28 +2,6 @@
   Небольшой, но очень важный
 */
 
-/*
-  Вешаем обработчк глобальных ошибок
-*/
-window.addEventListener('unhandledrejection',function(ev) {
-  emitError(ev.promise);
-  emitError(ev.reason);
-});
-
 function bootstrap() {
-  initMainToolbar();
-}
-
-function initMainToolbar() {
-  $.getScript('js/main-toolbar.js')
-    .done(function(script,textStatus){
-      initToolbar('mainToolbar','html/main-toolbar.html');
-    })
-    .fail(function(xhr,settings,exception){
-      _log_('Ошибка initMainToolbar.getScript:',settings,exception);
-    });
-}
-
-function refreshGallery() {
-  _log_('--- обновление галереи...');
+  app=new App();
 }
