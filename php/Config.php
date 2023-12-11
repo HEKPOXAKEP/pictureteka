2715d3a63aef507e821298cb59646aaf1eb0b636
Tosh Judeinov <hellish.engineer@gmail.com>
2023-12-11 04:08:32 +0300

    + method getConfData

--- php/Config.php
+++ php/Config.php
@@ -1,21 +1,37 @@
 <?php
 /*
-  ========================
-  Конфигурация программы
-  ========================
+  ==============================
+  Класс работы с конфигурацией
+  ==============================
 */
 require_once('TraitConfData.php');
 
 class Config {
-  use confData;
+  use confData; // include trait confData
 
   public function __construct() {
     // заполняем данные из _COOKIE
-    $this->galFName = $_COOKIE['gf'] ?? 'Galleries.php';
+    $this->galFName = $_COOKIE['gf'] ?? 'Gallery.conf.php';
     $this->galIdx = $_COOKIE['gi'] ?? 1;
     $this->pgNum = $_COOKIE['pg'] ?? 1;
-    $this->thSize = $_COOKIE['sz'] ?? 120;
-    $this->perPage = $_COOKIE['pp'] ?? 49;
+    $this->thSize = $_COOKIE['sz'] ?? 80;
+    $this->perPage = $_COOKIE['pp'] ?? 30;
+  }
+
+  /*
+    Вернёт данные в формате json для заполнения формы DlgConfig
+  */
+  public function getConfData($asJson=false) {
+    $a=[
+      'err'=>0, 'msg'=>'Ok',
+      'edit-galfname'=>$this->galFName,
+      'edit-thsize'=>$this->thSize,
+      'edit-perpage'=>$this->perPage,
+    ];
+    if ($asJson)
+      return json_encode($a);
+    else
+      return $a;
   }
 }
 ?>
