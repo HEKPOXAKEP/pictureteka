9d76b7243e052053526b6915490cc732d2060e51
Tosh Judeinov <hellish.engineer@gmail.com>
2023-12-11 04:19:31 +0300

    Added common functions: + setupCustomDlg + emptyDlg + closeDlg + showCustomDlg

--- js/main.js
+++ js/main.js
@@ -5,3 +5,40 @@
 function bootstrap() {
   app=new App();
 }
+
+function emptyDlg(dlgId) {
+  $(dlgId).empty();
+}
+
+function closeDlg(dlgId,emptyIt=true) {
+  $(dlgId).dialog('destroy');
+  if (emptyIt) emptyDlg(dlgId);
+}
+
+/*
+  Подготовка информационного диалога.
+  dlgType: error, warn, info
+*/
+function setupCustomDlg(dlgType) {
+  const
+    dlg=document.getElementById('msg-dlg');
+
+  dlg.className=dlgType;
+  dlg.innerHTML='';
+
+  return dlg;
+}
+
+function showCustomDlg(dlgType,msg,title) {
+  setupCustomDlg(dlgType).innerHTML=msg;
+
+  $('#msg-dlg').dialog({
+    'title': title ?? dlgType,
+    resizable: false,
+    modal: true,
+    width: 'auto',
+    buttons: [
+      {text: 'Ok', click: () => closeDlg('#msg-dlg')},
+    ]
+  });
+}
