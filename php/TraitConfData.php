4850e241185a001dde4012a7f555cc1cb71acb66
Tosh Judeinov <hellish.engineer@gmail.com>
2023-12-11 04:30:46 +0300

    Public fields

--- php/TraitConfData.php
+++ php/TraitConfData.php
@@ -5,10 +5,10 @@
   ===================================
 */
 trait  confData {
-  public string $galFName;  // имя файла описания галерей; def: Galleries.php
-  public int $galIdx;       // индекс текущей галереи в файле $this->gaFName
-  public int $pgNum;        // номер текущей страницы
-  public int $thSize;       // размер миниатюры
-  public int $perPage;      // к-во миниатюр на страницу
+  public string $galFName;    // имя файла описания галерей; def: Gallery.conf.php
+  public int $galIdx;         // индекс текущей галереи в файле $this->gaFName
+  public int $pgNum;          // номер текущей страницы
+  public int $thSize;         // размер миниатюры
+  public int $perPage;        // к-во миниатюр на страницу
 }
 ?>
