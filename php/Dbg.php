25db0fd7ad69f9350c31b6c741786b6532dcb0bd
Tosh Judeinov <hellish.engineer@gmail.com>
2023-12-11 04:11:07 +0300

    + Debugger class

---
+++ php/Dbg.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  ====================
+  Долбаггерный класс
+  ====================
+*/
+class Dbg
+{
+  static function _log(string $s) {
+    file_put_contents('__debug.log',$s.PHP_EOL,FILE_APPEND);
+  }
+
+  static function _json($a) {
+    file_put_contents(
+      '__debug.log',
+      json_encode($a,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT).PHP_EOL,FILE_APPEND);
+  }
+}
+
+$DBG=new Dbg();
+?>
+
