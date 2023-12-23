645c2f8ff865776126add6abde8033773cd91da0
Tosh Judeinov <hellish.engineer@gmail.com>
2023-12-23 05:12:41 +0300

    Added fillDlgData and related methods; + fixed dialog size 800x500 + styles for dialog

--- js/DlgSelectHall.js
+++ js/DlgSelectHall.js
@@ -12,11 +12,15 @@ class DlgSelectHall
   setupEvents(opts) {
     this.boundBtnOkClick=this.btnOkClick.bind(this);
     this.boundBtnCancelClick=this.btnCancelClick.bind(this);
+    this.boundBtnDescrClick=this.btnDescrClick.bind(this);
 
     opts.buttons=[
-      {text: 'Ok', id: 'btn-ok', click: this.boundBtnOkClick},
-      {text: 'Cancel', id: 'btn-cancel', click: this.boundBtnCancelClick}
+      {text: 'Ok', click: this.boundBtnOkClick},
+      {text: 'Отмена', click: this.boundBtnCancelClick}
     ];
+
+    opts.width=800;
+    opts.height=500;
   }
 
   async prepareDlg() {
@@ -62,12 +66,128 @@ class DlgSelectHall
     this.fillDlgData(halls);
   }
 
+  /*
+    @param {array} halls
+  */
   fillDlgData(halls) {
+    var
+      hh=[],
+      hallIdx=getCookie('hi'),
+      elHalls=document.getElementById('halls-list');
 
+    if (!hallIdx) {
+      hallIdx=1;
+      setCookie('hi',1);
+    }
+
+    elHalls.tabIndex=-1;
+
+    // create array from halls object
+    for (let i in halls) hh[i]=halls[i];
+
+    hh.forEach((hall,idx) => {
+      elHalls.appendChild(this.createHallElement(hall,idx,hallIdx));
+    });
+
+    this.bindEvents();
+    elHalls.focus();
+  }
+
+  createHallElement(hall,idx,hallIdx) {
+    const
+      h=document.createElement('div'),
+      r=this.createHallRadio(idx,hallIdx),
+      l=this.createHallLabel(hall,r.id),
+      d=this.createHallDescr(hall,idx),
+      b=this.createHallBtn(idx);
+
+    h.id='hall-'+idx.toString();
+    h.classList.add('hall-div');
+    if (hall['subhall']) h.classList.add('subhall');
+    h.append(r,l,b,d);
+
+    return h;
+  }
+
+  createHallRadio(idx,hallIdx) {
+    var
+      r=document.createElement('input');
+
+    r.id=`radio-${idx}`;
+    r.type='radio';
+    r.name='hall';
+    r.value=idx.toString();
+    if (idx ===Number(hallIdx)) r.checked=true;
+
+    return r;
+  }
+
+  createHallLabel(hall,forId) {
+    var
+      l=document.createElement('label');
+
+    l.setAttribute('for',forId);
+    l.innerHTML=(hall['subhall'] ? '&nbsp;&nbsp;' : '')+'&nbsp;&nbsp;&nbsp;'+hall['name'];
+
+    return l;
+  }
+
+  createHallBtn(idx) {
+    var
+      b=document.createElement('span');
+
+    b.id='btn-info-'+idx.toString();
+    b.classList.add('btn-info');
+    b.innerHTML='&#9668;';
+
+    return b;
+  }
+
+  createHallDescr(hall,idx) {
+    var
+      d=document.createElement('div');
+
+    d.id=`info-${idx}`;
+    d.classList.add('hall-info');
+    d.innerHTML=`<p>${hall['descr']}</p><p>${hall['path']}</p>`;
+
+    return d;
+  }
+
+  bindEvents() {
+    var
+      btns=document.querySelectorAll('#halls-list .btn-info');
+
+    Array.from(btns).forEach((btn) =>
+      btn.addEventListener('click',this.boundBtnDescrClick));
+  }
+
+  btnDescrClick(ev) {
+    ev.preventDefault();
+
+    const
+      // all arrow-buttons have an Id of the form "btn-info-nnn"
+      a=ev.target.id.split('-'),
+      d=document.getElementById(a.slice(-2,-1)+'-'+a.slice(-1));
+
+    if ((!d.style.display)||(d.style.display ==='none')) {
+      // show
+      d.style.display='block';
+      ev.target.innerHTML='&#9660;';
+    }  else {
+      // hide
+      d.style.display='none';
+      ev.target.innerHTML='&#9668;';
+    }
   }
 
   btnOkClick(ev) {
-    dlgCtrl.destroyDlg(this.dlg);
+    //dlgCtrl.destroyDlg(this.dlg);
+    dlgCtrl.showDlg(
+      'warn',
+      'Пока не реализовано, сорян. :-)',
+      'Under construction...'
+    );
   }
 
   btnCancelClick(ev) {
